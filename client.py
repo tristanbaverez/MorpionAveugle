bf1547ac815050e0ee651996f821d567ec606020
Nezout Carlos <eisti@student-laptop.portables.eisti.fr>
2017-01-06 20:54:17 +0100

    bon j'espère

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import sys
 
 hote = "" #vide par défault
 
-def connexion(port):
+def connexion(port, hote):
   connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   connexion_avec_serveur.connect((hote, port))
   print("Connexion établie avec le serveur sur le port {}".format(port))
@@ -33,6 +33,7 @@ def initialisationClient():
       print("Erreur : nombre d'arguments invalide")
     else:
       h=socket.gethostbyname(sys.argv[1])
+      print(h)
       p = int (input("Entrez le numéro de port svp (voir port serveur): \n"))
   except TypeError:
     print("Type incompatible")
@@ -41,6 +42,7 @@ def initialisationClient():
   else:
     #on suppose que le hostname est entré correctement
     port=p
-    connexion(p)
+
+    connexion(p,h)
 
 initialisationClient();
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from grid import *
 import socket
 import select
 
-hote = 'localhost'
+hote = '127.0.1.1'
 
 def connexion(port):
   connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
